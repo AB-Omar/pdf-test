382d7db444afda017d7da2a158c7b9112b8fe628
AB-Omar <abdulbasitomar9595@gmail.com>
2023-09-02 09:32:27 +0430

    feat: pdf file generate from html with images added

--- src/main/java/com/example/demo/controler/testController.java
+++ src/main/java/com/example/demo/controler/testController.java
@@ -1,16 +1,65 @@
 package com.example.demo.controler;
-
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.*;
+import java.nio.file.FileSystems;
+
+import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.xhtmlrenderer.layout.SharedContext;
 
 @RestController
 @RequestMapping (value = "")
 public class testController {
 
-    @GetMapping(value = "/test")
-    public String hello () {
-        return "Hello";
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @GetMapping(value = "/test", produces = MediaType.APPLICATION_PDF_VALUE)
+    @ResponseBody
+    public ResponseEntity<byte[]> generatePdf() {
+        try {
+            File htmlFile = new File("temp/index.html");
+            Document doc = Jsoup.parse(htmlFile, "UTF-8");
+            doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+
+            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+            ITextRenderer renderer = new ITextRenderer();
+            SharedContext sharedContext = renderer.getSharedContext();
+            sharedContext.setPrint(true);
+            sharedContext.setInteractive(false);
+
+            String baseUrl = FileSystems.getDefault().getPath("temp")
+                    .toUri().toURL().toString();
+
+            renderer.setDocumentFromString(doc.html(), baseUrl);
+            renderer.layout();
+            renderer.createPDF(pdfOutputStream);
+
+            pdfOutputStream.close();
+
+            byte[] pdfBytes = pdfOutputStream.toByteArray();
+
+            return ResponseEntity
+                    .ok()
+                    .contentLength(pdfBytes.length)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(pdfBytes);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity
+                    .badRequest()
+                    .body(null);
+        }
     }
-    
 }
