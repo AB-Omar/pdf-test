084c3aff19ec37f2454ca63e929cd1981c4695c0
AB-Omar <abdulbasitomar9595@gmail.com>
2023-09-04 17:08:43 +0430

    headers and footers

--- src/main/java/com/example/demo/controler/testController.java
+++ src/main/java/com/example/demo/controler/testController.java
@@ -5,14 +5,17 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
@@ -51,6 +54,10 @@ public class testController {
 
             byte[] pdfBytes = pdfOutputStream.toByteArray();
 
+            // Save the byte[] to a file in temp directory
+            try (FileOutputStream fos = new FileOutputStream("temp/docccc.pdf")) {
+                fos.write(pdfBytes);
+            }
             return ResponseEntity
                     .ok()
                     .contentLength(pdfBytes.length)
@@ -107,8 +114,8 @@ public class testController {
                 .body(null);
 
     }
-    
-    @GetMapping (value = "loadpdf")
+
+    @GetMapping(value = "loadpdf")
     public ResponseEntity<byte[]> loadPdf() throws IOException {
 
         File oldFile = new File("temp/loadpdf.pdf");
@@ -122,4 +129,103 @@ public class testController {
                 .created(null)
                 .body(null);
     }
+
+    @GetMapping(value = "mergepdf")
+    public ResponseEntity<byte[]> mergepdf() throws IOException {
+
+        File oldFile = new File("temp/loadpdf.pdf");
+        File oldFile1 = new File("temp/output.pdf");
+
+        PDDocument document = PDDocument.load(oldFile);
+        PDDocument document1 = PDDocument.load(oldFile1);
+
+        for (int i = 0; i < document1.getNumberOfPages(); i++) {
+            document.addPage(document1.getPage(i));
+        }
+        document.save("temp/newPdf.pdf");
+        System.out.println("testController.loadPdf()");
+        return ResponseEntity
+                .created(null)
+                .body(null);
+    }
+
+    @GetMapping(value = "addimage")
+    public ResponseEntity<byte[]> addImage(
+        @RequestParam("file") MultipartFile file,
+        @RequestParam("percentage") Integer percentage
+    ) throws IOException {
+        
+        final String headerText = "This is the header";
+        final String footerText = "This is the footer";
+        File oldFile = new File("temp/loadpdf.pdf");
+        PDDocument document = PDDocument.load(oldFile);
+    
+        int lastPageIndex = document.getNumberOfPages() - 1;
+        PDPage lastPage = document.getPage(lastPageIndex);
+    
+        PDImageXObject image = PDImageXObject.createFromByteArray(document, file.getBytes(), "added-image");
+    
+        float scale = (float) percentage / 100;
+        float newWidth = image.getWidth() * scale;
+        float newHeight = image.getHeight() * scale;
+    
+        float pageHeight = lastPage.getMediaBox().getHeight();
+        float pageWidth = lastPage.getMediaBox().getWidth();
+        
+        float yPosition = pageHeight - newHeight - 50; // start 50 units from the top
+        float xPosition = 50; // start 50 units from the left
+    
+        // Loop to add the image three times
+        for (int i = 0; i < 3; i++) {
+            // Check if the image fits vertically in the remaining space
+            if (yPosition - newHeight < 0) {
+                // Add a new page and reset positions
+                lastPage = new PDPage();
+                document.addPage(lastPage);
+                yPosition = pageHeight - newHeight - 50;
+                xPosition = 50;
+            }
+    
+            try (PDPageContentStream contentStream = new PDPageContentStream(document, lastPage, PDPageContentStream.AppendMode.APPEND, true, true)) {
+                contentStream.drawImage(image, xPosition, yPosition, newWidth, newHeight);
+            }
+    
+            yPosition -= (newHeight + 50);  // Move down for the next image, leaving 50 units as a gap
+        }
+    
+        addHeadersAndFooters(document);
+        document.save("temp/newPdf.pdf");
+    
+        return ResponseEntity
+                .created(null)
+                .body(null);
+    }
+    
+    public void addHeadersAndFooters(PDDocument document) throws IOException {
+        final String headerText = "This is the header";
+        final String footerText = "This is the footer";
+    
+        for (int i = 0; i < document.getNumberOfPages(); ++i) {
+            PDPage page = document.getPage(i);
+            float pageWidth = page.getMediaBox().getWidth();
+            float pageHeight = page.getMediaBox().getHeight();
+    
+            try (PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true, true)) {
+                // Header
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+                contentStream.beginText();
+                contentStream.newLineAtOffset(50, pageHeight - 50);  // position 50 units from top and left
+                contentStream.showText(headerText);
+                contentStream.endText();
+    
+                // Footer
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+                contentStream.beginText();
+                contentStream.newLineAtOffset(50, 50);  // position 50 units from bottom and left
+                contentStream.showText(footerText);
+                contentStream.endText();
+            }
+        }
+    }
+    
 }
